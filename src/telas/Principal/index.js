50832777a2673379a7e6a7c83e973f9928236c6c
andreocunha <andreoliveiracunha20@gmail.com>
2022-11-23 17:40:20 -0300

    aula 3.2

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, RefreshControl, ScrollView } from 'react-native';
 import Cabecalho from '../../componentes/Cabecalho';
 import Produto from '../../componentes/Produtos';
 import estilos from './estilos';
@@ -10,12 +10,16 @@ import { pegarProdutos } from '../../servicos/firestore';
 export default function Principal({ navigation }) {
   const usuario = auth.currentUser;
   const [produtos, setProdutos] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+
+  async function carregarDadosProdutos(){
+    setRefreshing(true)
+    const produtosFirestore = await pegarProdutos()
+    setProdutos(produtosFirestore)
+    setRefreshing(false)
+  }
 
   useEffect(() => {
-    async function carregarDadosProdutos(){
-      const produtosFirestore = await pegarProdutos()
-      setProdutos(produtosFirestore)
-    }
     carregarDadosProdutos()
   },[])
 
@@ -29,11 +33,21 @@ export default function Principal({ navigation }) {
       <Cabecalho logout={deslogar} />
       <Text style={estilos.texto}>Usuário: {usuario.email}</Text>
 
+      <ScrollView
+        style={{ width: '100%' }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={carregarDadosProdutos}
+          />
+        }
+      >
       {
-        produtos.map((produto) => {
+        produtos?.map((produto) => {
           return <Produto nome={produto.nome} preco={produto.preco} key={produto.id} />
         })
       }
+      </ScrollView>
       <BotaoProduto onPress={() => navigation.navigate("DadosProduto")} />
      </View>
   );
